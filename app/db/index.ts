ad693ea5b20aac452dc1e0c6cf42c1398622fbe7
KirdesMF <cedgourville@gmail.com>
2025-04-26 23:08:24 +0200

    fix: postgres driver

--- app/db/index.ts
+++ app/db/index.ts
@@ -1,8 +1,16 @@
 import { drizzle } from "drizzle-orm/postgres-js";
+import { neon } from "@neondatabase/serverless";
+import { drizzle as drizzleNeon } from "drizzle-orm/neon-http";
+import { drizzle as drizzlePg } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import { config } from "dotenv";
 
 config({ path: ".env" });
 
+// Check for Cloudflare Worker environment
+const isCloudflare = typeof process === "undefined";
+
 const client = postgres(process.env.DATABASE_URL as string, { prepare: false });
-export const db = drizzle({ client });
+export const db = isCloudflare
+  ? drizzleNeon(process.env.DATABASE_URL as string)
+  : drizzlePg(client);
