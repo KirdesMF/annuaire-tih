e8aac7f31b38415c4f666dc62660cf6538b8ad4d
KirdesMF <cedgourville@gmail.com>
2025-04-27 23:03:41 +0200

    deploy to workers successful but still error I/O

--- app/db/index.ts
+++ app/db/index.ts
@@ -1,16 +1,8 @@
 import { drizzle } from "drizzle-orm/postgres-js";
-import { neon } from "@neondatabase/serverless";
-import { drizzle as drizzleNeon } from "drizzle-orm/neon-http";
-import { drizzle as drizzlePg } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import { config } from "dotenv";
 
 config({ path: ".env" });
 
-// Check for Cloudflare Worker environment
-const isCloudflare = typeof process === "undefined";
-
 const client = postgres(process.env.DATABASE_URL as string, { prepare: false });
-export const db = isCloudflare
-  ? drizzleNeon(process.env.DATABASE_URL as string)
-  : drizzlePg(client);
+export const db = drizzle(client);
