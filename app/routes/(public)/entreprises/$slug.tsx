688597a681de3f2aaa07f46d858b031f31be4241
KirdesMF <cedgourville@gmail.com>
2025-05-05 23:34:40 +0200

    feat: container size company page

--- app/routes/(public)/entreprises/$slug.tsx
+++ app/routes/(public)/entreprises/$slug.tsx
@@ -1,12 +1,16 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { Link, createFileRoute } from "@tanstack/react-router";
-import { Globe, Globe2Icon, Mail, PencilLine, Phone, TwitterIcon, YoutubeIcon } from "lucide-react";
+import { Globe, Mail, PencilLine, Phone } from "lucide-react";
 import { CompanyLogo } from "~/components/company-logo";
 import { CopyButton } from "~/components/copy-button";
 import { CalendlyIcon } from "~/components/icons/calendly";
 import { FacebookIcon } from "~/components/icons/facebook";
 import { InstagramIcon } from "~/components/icons/instagram";
 import { LinkedinIcon } from "~/components/icons/linkedin";
+import { SpotifyIcon } from "~/components/icons/spotify";
+import { TiktokIcon } from "~/components/icons/tiktok";
+import { TwitterIcon } from "~/components/icons/twitter";
+import { YoutubeIcon } from "~/components/icons/youtube";
 import { Dialog, DialogContent, DialogTitle, DialogTrigger } from "~/components/ui/dialog";
 import { companyBySlugQuery } from "~/lib/api/companies/queries/get-company-by-slug";
 import { slugify } from "~/utils/slug";
@@ -33,9 +37,9 @@ const SOCIAL_MEDIA_ICONS = {
   linkedin: <LinkedinIcon className="size-5" />,
   calendly: <CalendlyIcon className="size-5" />,
   youtube: <YoutubeIcon className="size-5" />,
-  tiktok: <Globe2Icon className="size-5" />,
+  tiktok: <TiktokIcon className="size-5" />,
   twitter: <TwitterIcon className="size-5" />,
-  spotify: <Globe2Icon className="size-5" />,
+  spotify: <SpotifyIcon className="size-5" />,
 } as const;
 
 function RouteComponent() {
@@ -48,126 +52,128 @@ function RouteComponent() {
   const hasSocialMedia = Object.keys(data.social_media).length > 0;
 
   return (
-    <main className="px-4 py-8 grid gap-4">
-      <div className="container flex justify-between gap-4 border border-border bg-card text-card-foreground p-6 rounded-sm">
-        <div className="flex flex-col gap-2">
-          <CompanyLogo url={data.logo?.secureUrl} name={data.name} size="lg" />
-
-          <div className="flex items-center gap-2">
-            <h1 className="text-2xl font-bold">{data.name}</h1>
-            <CopyButton>{data.siret}</CopyButton>
-            {context.user?.id === data.user_id && (
-              <Link
-                to={"/compte/entreprises/$slug/edit/infos"}
-                params={{ slug: params.slug }}
-                className="border border-border bg-muted text-muted-foreground px-2 py-1.5 rounded-sm text-xs h-min flex items-center gap-1"
-              >
-                <span>Modifier</span>
-                <PencilLine className="size-4" />
-              </Link>
-            )}
-          </div>
-          <ul className="flex flex-wrap gap-2">
-            {data.categories.map((category) => (
-              <li
-                key={category?.id}
-                className="bg-accent text-accent-foreground px-2 py-1 rounded-sm text-xs"
-              >
-                <Link
-                  to="/categories/$slug"
-                  params={{ slug: slugify(category?.name ?? "") }}
-                  search={{ id: category?.id ?? "" }}
-                >
-                  {category?.name}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-
-      <div className="container flex gap-2 flex-wrap">
-        <div className="flex-1 flex flex-col justify-center gap-4 border border-border bg-card text-card-foreground p-6 rounded-sm">
+    <main className="px-4 py-8">
+      <div className="grid gap-4 max-w-5xl mx-auto py-24">
+        <div className="flex justify-between gap-4 border border-border bg-card text-card-foreground p-6 rounded-sm">
           <div className="flex flex-col gap-2">
-            <div className="flex items-center gap-2">
-              <Mail className="size-5" />
-              <p className="text-xs text-nowrap">{data.email || "Non renseigné"}</p>
-            </div>
+            <CompanyLogo url={data.logo?.secureUrl} name={data.name} size="lg" />
 
             <div className="flex items-center gap-2">
-              <Phone className="size-5" />
-              <p className="text-xs text-nowrap">{data.phone || "Non renseigné"}</p>
-            </div>
-
-            <div className="flex items-center gap-2">
-              <Globe className="size-5" />
-              {data.website ? (
-                <a
-                  href={data.website}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-xs text-nowrap"
+              <h1 className="text-2xl font-bold">{data.name}</h1>
+              <CopyButton>{data.siret}</CopyButton>
+              {context.user?.id === data.user_id && (
+                <Link
+                  to={"/compte/entreprises/$slug/edit/infos"}
+                  params={{ slug: params.slug }}
+                  className="border border-border bg-muted text-muted-foreground px-2 py-1.5 rounded-sm text-xs h-min flex items-center gap-1"
                 >
-                  {data.website}
-                </a>
-              ) : (
-                <span className="text-xs text-nowrap">Non renseigné</span>
+                  <span>Modifier</span>
+                  <PencilLine className="size-4" />
+                </Link>
               )}
             </div>
-          </div>
-
-          {hasSocialMedia ? (
-            <ul className="flex gap-2">
-              {Object.entries(data.social_media).map(([key, value]) => (
-                <li key={key}>
-                  <a href={value} target="_blank" rel="noopener noreferrer">
-                    {SOCIAL_MEDIA_ICONS[key as keyof typeof SOCIAL_MEDIA_ICONS]}
-                  </a>
+            <ul className="flex flex-wrap gap-2">
+              {data.categories.map((category) => (
+                <li
+                  key={category?.id}
+                  className="bg-accent text-accent-foreground px-2 py-1 rounded-sm text-xs"
+                >
+                  <Link
+                    to="/categories/$slug"
+                    params={{ slug: slugify(category?.name ?? "") }}
+                    search={{ id: category?.id ?? "" }}
+                  >
+                    {category?.name}
+                  </Link>
                 </li>
               ))}
             </ul>
-          ) : null}
+          </div>
         </div>
 
-        <div className="flex-1 border border-border bg-card text-card-foreground p-6 rounded-sm flex flex-col gap-2">
-          <p className="text-sm text-nowrap">
-            <span className="font-bold">Entrepreneur:</span>{" "}
-            {data.business_owner || "Non renseigné"}
-          </p>
-          <p className="text-sm text-nowrap">
-            <span className="font-bold">Zone géographique:</span>{" "}
-            {data.service_area || "Non renseigné"}
-          </p>
-          <p className="text-sm text-nowrap">
-            <span className="font-bold">Mode de travail:</span>{" "}
-            {WORK_MODES[data.work_mode ?? "not_specified"]}
-          </p>
-          <p className="text-sm text-nowrap">
-            <span className="font-bold">RQTH:</span> {data.rqth ? "Oui" : "Non"}
-          </p>
-          <p className="text-sm text-nowrap">
-            <span className="font-bold">Sous domaine:</span> {data.subdomain || "Non renseigné"}
-          </p>
+        <div className="flex gap-2 flex-wrap">
+          <div className="flex-1 flex flex-col justify-center gap-4 border border-border bg-card text-card-foreground p-6 rounded-sm">
+            <div className="flex flex-col gap-2">
+              <div className="flex items-center gap-2">
+                <Mail className="size-5" />
+                <p className="text-xs text-nowrap">{data.email || "Non renseigné"}</p>
+              </div>
+
+              <div className="flex items-center gap-2">
+                <Phone className="size-5" />
+                <p className="text-xs text-nowrap">{data.phone || "Non renseigné"}</p>
+              </div>
+
+              <div className="flex items-center gap-2">
+                <Globe className="size-5" />
+                {data.website ? (
+                  <a
+                    href={data.website}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-xs text-nowrap"
+                  >
+                    {data.website}
+                  </a>
+                ) : (
+                  <span className="text-xs text-nowrap">Non renseigné</span>
+                )}
+              </div>
+            </div>
+
+            {hasSocialMedia ? (
+              <ul className="flex gap-2">
+                {Object.entries(data.social_media).map(([key, value]) => (
+                  <li key={key}>
+                    <a href={value} target="_blank" rel="noopener noreferrer">
+                      {SOCIAL_MEDIA_ICONS[key as keyof typeof SOCIAL_MEDIA_ICONS]}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            ) : null}
+          </div>
+
+          <div className="flex-1 border border-border bg-card text-card-foreground p-6 rounded-sm flex flex-col gap-2">
+            <p className="text-sm text-nowrap">
+              <span className="font-bold">Entrepreneur:</span>{" "}
+              {data.business_owner || "Non renseigné"}
+            </p>
+            <p className="text-sm text-nowrap">
+              <span className="font-bold">Zone géographique:</span>{" "}
+              {data.service_area || "Non renseigné"}
+            </p>
+            <p className="text-sm text-nowrap">
+              <span className="font-bold">Mode de travail:</span>{" "}
+              {WORK_MODES[data.work_mode ?? "not_specified"]}
+            </p>
+            <p className="text-sm text-nowrap">
+              <span className="font-bold">RQTH:</span> {data.rqth ? "Oui" : "Non"}
+            </p>
+            <p className="text-sm text-nowrap">
+              <span className="font-bold">Sous domaine:</span> {data.subdomain || "Non renseigné"}
+            </p>
+          </div>
         </div>
+
+        <div className="border border-border bg-card text-card-foreground p-6 rounded-sm grid gap-4">
+          <div className="flex flex-col gap-2">
+            <h2 className="text-lg font-bold">Description</h2>
+            <p className="text-sm text-pretty whitespace-pre-line">
+              {data.description || "Non renseigné"}
+            </p>
+          </div>
+        </div>
+
+        {data.gallery?.length ? <GalleryImages gallery={data.gallery} /> : null}
       </div>
-
-      <div className="container border border-border bg-card text-card-foreground p-6 rounded-sm grid gap-4">
-        <div className="flex flex-col gap-2">
-          <h2 className="text-lg font-bold">Description</h2>
-          <p className="text-sm text-pretty whitespace-pre-line">
-            {data.description || "Non renseigné"}
-          </p>
-        </div>
-      </div>
-
-      {data.gallery?.length ? <GalleryImages gallery={data.gallery} /> : null}
     </main>
   );
 }
 
 function GalleryImages({ gallery }: { gallery: Array<{ secureUrl: string; publicId: string }> }) {
   return (
-    <div className="container border border-border bg-card text-card-foreground p-6 rounded-sm grid gap-4">
+    <div className="border border-border bg-card text-card-foreground p-6 rounded-sm grid gap-4">
       <h2 className="text-lg font-bold">Galerie</h2>
       <ul className="flex flex-wrap gap-4 items-center justify-center">
         {gallery.map((image) => {
