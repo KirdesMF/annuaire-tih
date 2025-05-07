697817611f43011e1f073932b8189dd113ecc419
KirdesMF <cedgourville@gmail.com>
2025-05-08 00:13:48 +0200

    remove dotenv

--- app/db/index.ts
+++ app/db/index.ts
@@ -1,4 +1,3 @@
-import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as authSchema from "./schema/auth";
@@ -6,8 +5,6 @@ import * as categoriesSchema from "./schema/categories";
 import * as cguSchema from "./schema/cgu";
 import * as companiesSchema from "./schema/companies";
 import * as companyCategoriesSchema from "./schema/company-categories";
-
-config({ path: ".env" });
 
 let _client: postgres.Sql | undefined;
 
