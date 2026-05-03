5f7e9d210f4314dadb9b69c8ad4117bb0ab5c129
KirdesMF <cedgourville@gmail.com>
2026-05-03 14:52:25 +0200

    refactor(ui): remove React namespace imports

--- app/components/ui/dropdown-menu.tsx
+++ app/components/ui/dropdown-menu.tsx
@@ -1,4 +1,4 @@
-import * as React from "react"
+import type { ComponentProps } from "react"
 import { Menu as MenuPrimitive } from "@base-ui/react/menu"
 
 import { cn } from "~/lib/utils"
@@ -129,7 +129,7 @@ function DropdownMenuSubContent({
   sideOffset = 0,
   className,
   ...props
-}: React.ComponentProps<typeof DropdownMenuContent>) {
+}: ComponentProps<typeof DropdownMenuContent>) {
   return (
     <DropdownMenuContent
       data-slot="dropdown-menu-sub-content"
@@ -234,7 +234,7 @@ function DropdownMenuSeparator({
 function DropdownMenuShortcut({
   className,
   ...props
-}: React.ComponentProps<"span">) {
+}: ComponentProps<"span">) {
   return (
     <span
       data-slot="dropdown-menu-shortcut"
